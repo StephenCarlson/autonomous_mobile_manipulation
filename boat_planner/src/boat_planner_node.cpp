64437821edbed468dad6d1f1528ede8946dd51af
Steve Carlson <stephen_carlson@byu.net>
2021-04-26 22:13:49 -0700

    Initial commit of boat_planner

---
+++ boat_planner/src/boat_planner_node.cpp
@@ -0,0 +1,26 @@
+# include <ros/ros.h>
+
+// #include <boat_planner/boat_planner.h>
+
+int main(int argc, char* argv[])
+{
+
+    ros::init(argc, argv, "boat_planner");
+
+    // ros::AsyncSpinner spinner(2); // TODO: Examples have 1, but move_group fails with: "Didn't received robot state (joint angles) with recent timestamp within 1 seconds." (see: https://github.com/ros-planning/moveit/issues/868)
+    // spinner.start();
+
+    // ros::NodeHandle nh;
+    // ros::NodeHandle pnh("~");
+
+    // move_group_interface::MGI_AprilTag move_group_interface_AprilTag(nh, pnh, "map");
+  
+    // ros::waitForShutdown();
+
+
+    ROS_INFO("boat_planner is properly compiled if this is visible.");
+
+    ros::spin();
+
+    return 0;
+}
